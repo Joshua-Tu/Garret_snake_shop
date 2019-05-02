e7486a05ce1189c4dbec6e963cdd07a9bcb9bbac
Joshua-Tu <tjh8523@gmail.com>
2019-05-03 02:48:21 +1000

    added controllers and actions, plus views for listings.

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,29 @@
+class ListingController < ApplicationController
+  def create
+    # create new listing
+  end
+
+  def update
+    #updates the current listing
+  end
+
+  def index
+    #show all listings
+  end
+
+  def edit
+    #shows the edit forms
+  end
+
+  def destroy
+    #deletes current listing
+  end
+
+  def new
+    #shows forms for creating a new listing
+  end
+
+  def show 
+    #view a single listing
+  end  
+end
--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,2 +1,3 @@
 class Breed < ApplicationRecord
+  has_many :listing #here we don't add dependent: : destroy, it's because we don't want some one to stupidly delete our breeds.
 end
