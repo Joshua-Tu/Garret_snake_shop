e4edfb8885a7d7ffcc4ba132dab2f076f3a2db42
Joshua-Tu <tjh8523@gmail.com>
2019-05-05 23:31:20 +1000

    added in stripe gemand modified show view with deposit button

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [ :index, :show]
+  before_action :set_listing, only: [:show,:edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
   before_action :set_breeds_and_sexes, only: [:new, :edit]
   def index
     #show all listings
@@ -13,7 +14,7 @@ class ListingsController < ApplicationController
 
   def create
     # create new listing
-    @listing = Listing.create(listing_params)
+    @listing = current_user.listings.create(listing_params)
     # byebug
     if @listing.errors.any?
       set_breeds_and_sexes
@@ -24,15 +25,12 @@ class ListingsController < ApplicationController
     
   end
 
-  def listing_params
-    params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture )
-  end
-
   def update
     #updates the current listing
   end
 
   def edit
+
     #shows the edit forms
   end
 
@@ -58,5 +56,23 @@ class ListingsController < ApplicationController
     @listing = Listing.find(id)
   end
 
- 
+ def set_user_listing
+  #method 1
+  # @listing = Listing.where(user_id: current_user.id, id: params[:id]).first
+  # if !@listing
+  #   redirect_to listings_path 
+  # end
+
+ end
+
+  #method 2
+ def authorize_user
+  if @listing.user_id != current_user.id
+    redirect_to listings_path
+  end
+ end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet, :picture )
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ApplicationRecord
   enum sex:{female: 0, male: 1}
   validates :title, :breed_id, :sex, :price, :deposit, :date_of_birth, presence: true # presence is the validation rule, we didnt put :diet here, this means that the users do not necessarily input diet value
   has_one_attached :picture
+  belongs_to :user
 end
